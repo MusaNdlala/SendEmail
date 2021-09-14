0af954fec1343d42236e2923d30fccce678f1306
Musa Ndlala <musandlala@yahoo.com>
2021-09-14 14:31:13 +0200

    The email now sends with pictures but stilll hardcoded. Will make

--- SendEmail/EmailSend.cs
+++ SendEmail/EmailSend.cs
@@ -7,25 +7,25 @@ namespace SendEmail
 {
     sealed class EmailSend
     {
-        public bool SendEmail(EmailDetails sendEmail, bool sslOn_Of = true) 
+        public bool SendEmail(EmailDetails sendEmail, bool sslOn_Of = true)
         {
             try {
-                return SendEmail(sendEmail,sslOn_Of,null);
+                return SendEmail(sendEmail, sslOn_Of, null);
             }
-            catch (Exception){
+            catch (Exception) {
                 return false;
             }
         }
         public bool SendEmail(EmailDetails sendEmail, Attached attached = null)
         {
-            try { 
-                return SendEmail(sendEmail,true,attached);
+            try {
+                return SendEmail(sendEmail, true, attached);
             }
-            catch (Exception){ 
+            catch (Exception) {
                 return false;
             }
         }
-        public bool SendEmail(EmailDetails sendEmail, bool sslOn_Of=true,Attached attached=null)
+        public bool SendEmail(EmailDetails sendEmail, bool sslOn_Of = true, Attached attached = null)
         {
             try
             {
@@ -43,12 +43,14 @@ namespace SendEmail
                     IsBodyHtml = true,
                 };
                 mailMessage.To.Add(sendEmail.RecievingEmail);
-                AlternateView alternativeView = AlternateView.CreateAlternateViewFromString(sendEmail.body, null, MediaTypeNames.Text.Html);
-                //alternativeView.LinkedResources.Add(iconResource);
-                mailMessage.AlternateViews.Add(alternativeView);
 
-                if (attached !=null) {
-                    if(attached.MediaType == null)
+                var pathurl = @"C:\Users\Musa\source\repos\SendEmail\SendEmail\html\EmailTEmplate1\images\email.png";
+                mailMessage = EmbedPictures(mailMessage, pathurl, sendEmail.body);
+                if (mailMessage == null)
+                    return false;
+
+                if (attached != null) {
+                    if (attached.MediaType == null)
                         attached.MediaType = MediaTypeNames.Text.Plain;
 
                     var attachment = new Attachment(attached.url, attached.MediaType);
@@ -64,6 +66,32 @@ namespace SendEmail
                 Console.WriteLine(e.Message);
                 return false;
             }
+        }
+        private MailMessage EmbedPictures(MailMessage temp,string imageUrl, string htmlBody)
+        {
+            try
+            {
+                var picturez = new Attached(imageUrl);
+                var attachment2 = new Attachment(picturez.url, picturez.MediaType);
+                AlternateView alternativeView = GetEmbeddedImage(picturez.url, htmlBody);
+                temp.AlternateViews.Add(alternativeView);
+                return temp;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        private AlternateView GetEmbeddedImage(String filePath, string EmailBody)
+        {
+            LinkedResource res = new LinkedResource(filePath);
+            res.ContentId = Guid.NewGuid().ToString();
+
+            string htmlBody = EmailBody.Replace("[LogoImage]", "cid:" + res.ContentId);
+            
+            AlternateView alternateView = AlternateView.CreateAlternateViewFromString(htmlBody, null, MediaTypeNames.Text.Html);
+            alternateView.LinkedResources.Add(res);
+            return alternateView;
         }
     }
 }
